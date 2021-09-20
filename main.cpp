dcabd72690605abd7a2c4ddd5aa8d900ed055510
xiaogavin-hunter <gavin.xiao89@myhunter.cuny.edu>
2021-09-20 12:05:24 -0400

    Completed all tasks, A - G

--- main.cpp
+++ main.cpp
@@ -12,7 +12,9 @@ describe what the program does.
 #include "shapes.h"
 
 int main() {
-    // box(width, height); 
+    // box(width, height); Task A
+    std::cout << "Task A:\n";
+
     std::cout << "box(3,5):\n";
     std::cout << box(3,5);
 
@@ -24,7 +26,9 @@ int main() {
 
     std::cout << "\n------------------\n"; // seperator
 
-    // checkerboard(width, height); 
+    // checkerboard(width, height); Task B
+    std::cout << "Task B:\n"; 
+
     std::cout << "checkerboard(11, 6):\n";
     std::cout << checkerboard(11, 6);
 
@@ -36,5 +40,71 @@ int main() {
 
     std::cout << "\n------------------\n"; // seperator
 
+    // cross(size); Task C
+    std::cout << "Task C:\n";
+
+    std::cout << "cross(5):\n"; 
+    std::cout << cross(5); 
+
+    std::cout << "cross(8):\n"; 
+    std::cout << cross(8); 
+
+    std::cout << "cross(15):\n"; 
+    std::cout << cross(15); 
+
+    std::cout << "\n------------------\n"; // seperator
+
+    // lower(length): Task D
+    std::cout << "Task D:\n";
+
+    std::cout << "lower(1):\n"; 
+    std::cout << lower(1); 
+
+    std::cout << "lower(4)\n";
+    std::cout << lower(4); 
+
+    std::cout << "lower(7)\n"; 
+    std::cout << lower(7);
+
+    std::cout << "\n------------------\n"; // seperator
+
+    // upper(length): Task E
+    std::cout << "Task E:\n";
+
+    std::cout << "upper(1):\n";
+    std::cout << upper(1);
+
+    std::cout << "upper(4):\n";
+    std::cout << upper(4);
+    
+    std::cout << "upper(7):\n";
+    std::cout << upper(7);
+
+    std::cout << "\n------------------\n"; // seperator
+
+    // trapezoid(width, height): Task F
+    std::cout << "Task F:\n";
+
+    std::cout << "trapezoid(12, 5):\n"; 
+    std::cout << trapezoid(12, 5);
+
+    std::cout << "trapezoid(5, 3):\n";
+    std::cout << trapezoid(5, 3);
+
+    std::cout << "trapezoid(12, 7):\n";
+    std::cout << trapezoid(12, 7);
+
+    std::cout << "\n------------------\n"; // seperator
+ 
+    // checkerboard3x3(width, height): Task G
+    std::cout << "Task G:\n";
+    std::cout << "checkerboard3x3(4, 4):\n";
+    std::cout << checkerboard3x3(4,4);
+
+    std::cout << "checkerboard3x3(6, 8):\n";
+    std::cout << checkerboard3x3(6,8);
+
+    std::cout << "checkerboard3x3(27, 27):\n";
+    std::cout << checkerboard3x3(27,27);
 return 0;
 }
